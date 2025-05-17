341aa021a59b2d319ee57f6b031df71112186238
jin20fd <syj010820@naver.com>
2025-05-17 18:14:56 +0900

    swagger 설정 추가

---
+++ src/main/java/com/everybox/everybox/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.everybox.everybox.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("모두의 냉장고")
+                .version("1.0.0");
+    }
+}
