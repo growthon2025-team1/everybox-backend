a5f7e0fa180df3e5943abc36848348c55e426718
jin20fd <syj010820@naver.com>
2025-05-17 18:22:45 +0900

    swagger Authorize 헤더 설정 추가

--- src/main/java/com/everybox/everybox/config/SwaggerConfig.java
+++ src/main/java/com/everybox/everybox/config/SwaggerConfig.java
@@ -1,16 +1,30 @@
 package com.everybox.everybox.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(apiInfo());
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("BearerAuth")
+                );
     }
 
     private Info apiInfo() {
