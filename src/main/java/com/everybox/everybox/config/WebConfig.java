17e94e3c970a4e832c0e4cc8f99529952d4a291b
jin20fd <syj010820@naver.com>
2025-05-21 15:30:30 +0900

    fix: CORS 설정 변경

--- src/main/java/com/everybox/everybox/config/WebConfig.java
+++ src/main/java/com/everybox/everybox/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true);
     }
 }
